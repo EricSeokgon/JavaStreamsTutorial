4dd2febd645cfb250d54e69a610e9e5ff30f3008
hadesyi <leesk55@gmail.com>
2017-09-13 23:51:05 +0900

    Sequential

---
+++ src/operation/Sequntial.java
@@ -0,0 +1,19 @@
+package operation;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by hadeslee on 2017-09-13.
+ */
+public class Sequntial {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        int sum = numbers.parallelStream()
+                .filter(n -> n % 2 == 1)
+                .map(n -> n * n)
+                .reduce(0, Integer::sum);
+        System.out.println(sum);
+    }
+}
