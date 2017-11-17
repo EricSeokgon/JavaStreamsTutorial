f3291830a9d1b4b23e90a5edee5eda0a59f55a78
hadesyi <leesk55@gmail.com>
2017-11-18 01:23:44 +0900

    Streams Peek

---
+++ src/stream_operations/Streams_Peek.java
@@ -0,0 +1,18 @@
+package stream_operations;
+
+import java.util.stream.Stream;
+
+/**
+ * Created by hadeslee on 2017-11-18.
+ */
+public class Streams_Peek {
+    public static void main(String[] args) {
+        int sum = Stream.of(1, 2, 3, 4, 5)
+                .peek(e -> System.out.println("Taking integer: " + e))
+                .filter(n -> n % 2 == 1)
+                .peek(e -> System.out.println("Filtered integer: " + e))
+                .map(n -> n * n).peek(e -> System.out.println("Mapped integer: " + e))
+                .reduce(0, Integer::sum);
+        System.out.println("Sum = " + sum);
+    }
+}
