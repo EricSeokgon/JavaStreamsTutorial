4e2570cccda5a5449b5245571426f39bd2981746
hadesyi <leesk55@gmail.com>
2017-12-24 05:44:07 +0900

    ava Stream Map Reduce in parallel

---
+++ src/Stream_reduce/MapRecudeinParallel.java
@@ -0,0 +1,50 @@
+package Stream_reduce;
+
+import stream_operations.Employee;
+
+public class MapRecudeinParallel {
+    public static void main(String[] args) {
+        double sum = Employee
+                .persons()
+                .stream()
+                .reduce(
+                        0.0,
+                        (Double partialSum, Employee p) -> {
+                            double accumulated = partialSum + p.getIncome();
+                            System.out.println(Thread.currentThread().getName()
+                                    + "  - Accumulator: partialSum  = " + partialSum
+                                    + ",  person = " + p + ", accumulated = " + accumulated);
+                            return accumulated;
+                        },
+                        (a, b) -> {
+                            double combined = a + b;
+                            System.out.println(Thread.currentThread().getName()
+                                    + "  - Combiner:  a  = " + a + ", b  = " + b
+                                    + ", combined  = " + combined);
+                            return combined;
+                        });
+        System.out.println("--------------------------------------");
+        System.out.println(sum);
+
+        sum = Employee
+                .persons()
+                .parallelStream()
+                .reduce(
+                        0.0,
+                        (Double partialSum, Employee p) -> {
+                            double accumulated = partialSum + p.getIncome();
+                            System.out.println(Thread.currentThread().getName()
+                                    + "  - Accumulator: partialSum  = " + partialSum
+                                    + ",  person = " + p + ", accumulated = " + accumulated);
+                            return accumulated;
+                        },
+                        (a, b) -> {
+                            double combined = a + b;
+                            System.out.println(Thread.currentThread().getName()
+                                    + "  - Combiner:  a  = " + a + ", b  = " + b
+                                    + ", combined  = " + combined);
+                            return combined;
+                        });
+        System.out.println(sum);
+    }
+}
